ba82390c47b420f3952c9d3179a7dd28c572e972
Robiul Hossain <robiulhossain0110@gmail.com>
2025-08-27 21:19:44 +0600

    new

--- models/User.js
+++ models/User.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 
-<<<<<<< HEAD
 // User Schema
 const UserSchema = new mongoose.Schema(
   {
@@ -25,15 +24,6 @@ const UserSchema = new mongoose.Schema(
     timestamps: true, // createdAt, updatedAt automatically
     versionKey: false, // remove __v field
   }
-=======
-const userSchema = new mongoose.Schema(
-  {
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-  },
-  { timestamps: true }
->>>>>>> 177cb9c3252395a9fbf1d72cf40b9e5b66aae429
 );
 
 // Hide password when converting to JSON
